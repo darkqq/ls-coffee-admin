d74d77ff600e2d7ad573fc7a8f9efd9ff6a03be4
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-04-17 19:27:07 +0300

    feat: add components button and menu-item, reset styles and add Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-
+import IconDashboard from "./Icons/IconDashboard";
+import MenuItem from "./components/MenuItem/MenuItem";
+import IconPeoples from "./Icons/IconPeoples";
+import IconsAdministratin from "./Icons/IconsAdministratin";
 
 function App() {
   return (
     <div className="App">
-
+        <MenuItem icon={<IconDashboard/>} title={"Title"}/>
+        <MenuItem icon={<IconsAdministratin/>} title={"Title"}/>
     </div>
   )
 }
---
+++ src/Icons/IconCalendar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IconCalendar = () => {
+	return (
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M17.2502 13.25H12.7502V17.75H17.2502V13.25ZM15.7502 3.5V5H8.25023V3.5H6.00023V5H4.87476C3.84341 5 2.99976 5.84366 2.99976 6.875V19.625C2.99976 20.6563 3.84341 21.5 4.87476 21.5H19.1252C20.1566 21.5 21.0002 20.6563 21.0002 19.625V6.875C21.0002 5.84366 20.1566 5 19.1252 5H18.0002V3.5H15.7502ZM19.1252 19.625H4.87476V9.6875H19.1252V19.625Z" fill="currentColor"/>
+		</svg>
+
+	);
+};
+
+export default IconCalendar;
